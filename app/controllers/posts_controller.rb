e7c0f99c4859758330900d8885e8958bda45a2fe
Berry  Semexan <bsemexan@gmail.com>
2018-04-17 05:53:43 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
-  def update
-    @post = Post.find(params[:id])
-    return head(:forbidden) unless current_user.admin? || current_user.vip?
-    @post.update(post_params)
-    redirect_to post_path(@post)
+  before_action :authenticate_user!
+  before_action :find_post!, only: [:edit, :show, :update]
+  before_action :authorize_user!, only: [:edit, :update]
+
+  def create
+    user_id = current_user.id
+
+    Post.create(
+      content: params[:post][:content],
+      user_id: user_id
+    )
+
+    redirect_to user_path(id: user_id)
+  end
+
+  def edit
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
-  def index
+  def update
+    @post.update!(content: params[:post][:content])
+
+    redirect_to post_path, id: @post.id
   end
 
   private
 
-  def post_params
-    params.require(:post).permit(:content)
+  def authorize_user!
+    raise "Unauthorized!" unless current_user
+
+    unless current_user == @post.user || current_user.vip? || current_user.admin?
+      redirect_to post_path(id: @post.id), :alert => "Access denied."
+    end
+  end
+
+  def find_post!
+    @post = Post.find(params[:id])
   end
 end
